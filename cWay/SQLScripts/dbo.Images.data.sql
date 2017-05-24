46692db4e021c7aa4c15d87c9c0419b41b2c1d6a
tmfahall <tmfahall@gmail.com>
2017-05-24 14:30:01 -0500

    added sql scripts

---
+++ cWay/SQLScripts/dbo.Images.data.sql
@@ -0,0 +1,11 @@
+﻿SET IDENTITY_INSERT [dbo].[Images] ON
+INSERT INTO [dbo].[Images] ([ImageID], [ImageFileName], [TruckID]) VALUES (1, N'245df562-b72e-422f-a67e-945bb06f318a.png', 2)
+INSERT INTO [dbo].[Images] ([ImageID], [ImageFileName], [TruckID]) VALUES (2, N'0a9cb783-e3c7-4751-a47b-eb590cb24fc1.png', 2)
+INSERT INTO [dbo].[Images] ([ImageID], [ImageFileName], [TruckID]) VALUES (3, N'2759d9b7-cab8-4cbb-ab7a-14975eb39df5.png', 2)
+INSERT INTO [dbo].[Images] ([ImageID], [ImageFileName], [TruckID]) VALUES (4, N'cb0f1c0d-1cb5-4688-838d-a23ed8127722.png', 2)
+INSERT INTO [dbo].[Images] ([ImageID], [ImageFileName], [TruckID]) VALUES (5, N'b8e7e056-15fb-4191-8fd2-a8e7077dc6fe.jpg', 3)
+INSERT INTO [dbo].[Images] ([ImageID], [ImageFileName], [TruckID]) VALUES (6, N'f7c4fec4-8443-47c7-8100-eeba3e382063.jpg', 4)
+INSERT INTO [dbo].[Images] ([ImageID], [ImageFileName], [TruckID]) VALUES (7, N'96e1e7ed-3492-4c36-bf20-750b94637c74.jpg', 5)
+INSERT INTO [dbo].[Images] ([ImageID], [ImageFileName], [TruckID]) VALUES (8, N'f118d3f0-846b-4bb7-a8d1-7f293f2e8b64.jpg', 6)
+INSERT INTO [dbo].[Images] ([ImageID], [ImageFileName], [TruckID]) VALUES (9, N'1fea2ad3-880f-47ec-a08c-59f1897470a3.jpg', 7)
+SET IDENTITY_INSERT [dbo].[Images] OFF
